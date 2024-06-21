04218f29481eb4053cef1e824e267ab0e53a4a92
Max <max.kolts2003@gmail.com>
2024-06-21 08:28:07 +0300

    bugfix

--- app/src/utils/database_filling.py
+++ app/src/utils/database_filling.py
@@ -194,7 +194,7 @@ async def database_filling(con: AsyncConnection):
     ))
 
     start = datetime.datetime.strptime("11-01-2024", "%d-%m-%Y")
-    end = datetime.datetime.strptime("25-06-2024", "%d-%m-%Y")
+    end = datetime.datetime.strptime("20-07-2024", "%d-%m-%Y")
     date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days)]
 
     for date in date_generated:
